6b5a3d779419d1b7344ce47ab52d9c9939eb4412
Preethi H P <preethihp1802@gmail.com>
2024-08-09 15:02:15 +0530

    Update Index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ document.getElementById('event-form').addEventListener('submit', function(event)
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    Moengage.add_email(email);
-    Moengage.add_first_name(name);
+    Moengage.add_email("test@gmail.com");
+    Moengage.add_first_name("Preethi");
 
     const eventName = "Form_Submitted";
     
